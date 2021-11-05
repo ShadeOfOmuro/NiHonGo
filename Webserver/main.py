3a2eb2ac621106ffef2e938e7699803a2e924c4e
Shironi-dev <shironi.studio@gmail.com>
2021-11-05 22:30:13 +0700

    updated base for main.py for the server side

--- Webserver/main.py
+++ Webserver/main.py
@@ -1,1 +1,9 @@
-# Server Side Code Begins here
+from typing import Optional
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/get_word_set/ping")
+def read_root():
+    return {"status": "I'm alive"}
+

ead8155ee14188e445e20351e939abe2f4bca96c
Shironi-dev <shironi.studio@gmail.com>
2021-11-25 10:35:00 +0700

    rework on main.py

--- Webserver/main.py
+++ Webserver/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
-import sqlite3
 from pydantic import BaseModel
+
 app = FastAPI()
 
 class LoginData(BaseModel) :
@@ -12,20 +12,4 @@ async def root():
 
 @app.post("/login")
 async def login(data : LoginData):
-    conn = sqlite3.connect("MainDataBase.sqlite")
-    print("Connection Succeed!")
-    curr = conn.cursor()
-    print(data.username,data.password)
-    curr.execute("SELECT uid,username FROM users WHERE username=? AND pwd=?",(data.username,data.password))
-    print(curr.rowcount)
-    if  curr.rowcount != -1 :
-        x = curr.fetchone()
-        return {
-            "uid" : x[0],
-            "username" : x[1]
-        }
-    else :
-        return {
-            "uid" : "not found",
-            "username" : "not found"
-        }
+    return {"status", " working"}
